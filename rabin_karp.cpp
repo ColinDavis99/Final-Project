e4040c7d879d1bbe0284d8fe6d0be241a248e94c
seanlmaglione <sean_maglione@my.uri.edu>
2020-11-23 20:37:17 +0000

    implemented hash and search with substrings

--- rabin_karp.cpp
+++ rabin_karp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
 #include "rabin_karp.h"
 
 Rabin_Karp::Rabin_Karp (std::string inputString, std::string searchString, bool isFile) { // constructor
@@ -14,13 +15,65 @@ Rabin_Karp::Rabin_Karp (std::string inputString, std::string searchString, bool 
 // Rabin1.search();
 // Rabin2.search();
 
-int Rabin_Karp::hash(std::string stringToHash) { // find the hash of a string or substring
-    return 23;
+std::string Rabin_Karp::getSearchWindow() {
+    return searchWindow;
 }
 
-void Rabin_Karp::search() { // performs the actual string search
-    std::cout << "Hello World" << std::endl;
+void Rabin_Karp::setSearchWindow(std::string searchWindow) {
+    this->searchWindow = searchWindow;
 }
+
+long long Rabin_Karp::hash(std::string stringToHash) { // find the hash of a string or substring
+    //NOTE: This is a polynomial rolling hash function
+    const int p_base = 97; // prime base. since there are 95 possible characters and 97 is the closest prime number
+    const int d_base = 1000000007; // dividend base (10^9 + 7, a large prime number)
+    int p_pow = 1; // The power of the prime number
+    long long hashVal = 0;
+    for (int i = 0; i < stringToHash.length(); i++) {
+        hashVal = (hashVal +  (stringToHash[i] - 'a' + 1) * p_pow) % d_base;
+        p_pow = (p_pow * p_base) % d_base;
+    }
+
+    return hashVal;
+}
+
+void Rabin_Karp::output(std::string searchString, std::list<int> foundIndexes, int count) {
+    if (foundIndexes.size() == 1) {
+        std::cout << "The string " << searchString << " was found at index " << foundIndexes.front() << "." << std::endl;
+        std::cout << "It was found " << count << " time." << std::endl;
+    } else {
+        std::string outputIndexes = "The string ";
+        outputIndexes += searchString;
+        outputIndexes += " was found at indexes ";
+        for(std::list<int>::iterator i = foundIndexes.begin(); i != foundIndexes.end(); ++i) {
+            outputIndexes += std::to_string(*i) + ", ";
+        }
+        std::cout << outputIndexes << std::endl;
+        std::cout << "It was found " << count << " times." << std::endl;
+    }
+
+}
+
+
+void Rabin_Karp::search() { // performs the actual string search
+    std::list<int> foundIndexes; // list of the indexes where the search string was found
+    int count = 0; // count the number of times searchString is found in inputString
+
+    if (!isFile) {
+        long long searchHash = hash(searchString);
+        int searchStringLength = searchString.length();
+        for (int i = 0; i + searchStringLength - 1 < inputString.length(); i++) { // goes until the length of the string is smaller than a search window
+            searchWindow = inputString.substr(i, searchStringLength);
+            if (hash(searchWindow) == searchHash) {
+                foundIndexes.push_back(i);
+                count++;
+            }
+        }
+    }
+
+    output(searchString, foundIndexes, count);
+}
+
 
 //Rabin_Karp::hash("asiejfwojef")
 //std::cout
--- rabin_karp.h
+++ rabin_karp.h
@@ -3,16 +3,21 @@
 
 #include <fstream>
 #include <string>
+#include <list>
 
 class Rabin_Karp {
      private :
         std::string inputString; // can be filename or string
-        std::string searchString; //string you are searching for will not change
-        bool isFile;
+        std::string searchString; //string you are searching for
+        bool isFile; // determines if string describes the name of a file or just a search string
+        std::string searchWindow;
 
      public :
         Rabin_Karp(std::string inputString, std::string searchString, bool isFile); // constructor for looking through a txt file
-        static int hash(std::string stringToHash); //find the hash value of a string or substring
+        std::string getSearchWindow();
+        void setSearchWindow(std::string searchWindow);
+        static long long hash(std::string stringToHash); //find the hash value of a string or substring
+        static void output(std::string searchString, std::list<int> foundIndexes, int count);
         void search(); //search the file or input string for the search string
 };
 

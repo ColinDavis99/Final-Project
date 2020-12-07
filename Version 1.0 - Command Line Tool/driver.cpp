bd6c8ba79c4db6d32902d6e3a65d2b520e723666
seanlmaglione <60487674+seanlmaglione@users.noreply.github.com>
2020-12-07 17:49:50 -0500

    Add files via upload

---
+++ Version 1.0 - Command Line Tool/driver.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include "string_search_I.h"
+
+//Driver Code & Test Cases
+
+// Making an object:
+// [Rabin_Karp/Boyer_Moore] [Object Name](input file or string, what you want to search for, is it a file input?, is it case sensitive?)
+// Example: Rabin_Karp Rabin_Karp1("AbCdEfG", "AbCdEfG", false, false);
+// Operation: Will search for "ABC" in "AbCdEfG", it is not a file, it will not worry about capial letters
+// Result: It will be found 1 time
+// -----------------------
+// Running a Search:
+// [Object Name].search(do you want to supress the output to the terminal?)
+// Example Rabin_Karp1.search(true);
+// Operation: Will not spam the terminal with count and indexes. (useful when calculating average runtime)
+
+void getAverageRunTime(Boyer_Moore Boyer_Moore_Obj, int numTrials);
+
+void getAverageRunTime(Boyer_Moore Boyer_Moore_Obj, int numTrials) {
+
+    int mean = 0;
+    int i = 0;
+    Boyer_Moore_Obj.search(true);
+    int temptime = Boyer_Moore_Obj.getRunTime();
+    while (i != numTrials) {
+        Boyer_Moore_Obj.search(true);
+        if (temptime >= Boyer_Moore_Obj.getRunTime()*1.3) { // This indicates a bad first test case, try again.
+            getAverageRunTime(Boyer_Moore_Obj, numTrials);
+            return;
+        }
+        if (Boyer_Moore_Obj.getRunTime() <= 1.3*temptime) {
+            mean += Boyer_Moore_Obj.getRunTime();
+            i++;
+        }
+
+    }
+
+    mean /= numTrials;
+
+    std::cout << "The mean runtime for " << Boyer_Moore_Obj.getTag() << " is " << mean << " microseconds" << std::endl;
+}
+
+int main() {
+
+    //Rabin_Karp Rabin_Karp1("WELCOMETOONLINECOLLEGE", "COLLEGE", false, true);
+    //Rabin_Karp1.search(false);
+
+    //Boyer_Moore Boyer_Moore1("WELCOMETOONLINECOLLEGE", "COLLEGE", false, true); // makes a boyer_moore object
+    //Boyer_Moore1.search(false); // runs the boyer_moore search algorith on the object
+
+    Boyer_Moore Boyer_Moore2("blackhat2014.txt", "the", true, false);
+    //Boyer_Moore2.search(true);
+
+    Rabin_Karp Rabin_Karp2("blackhat2014.txt", "the", true, false);
+    //Rabin_Karp2.search(true);
+
+    //EXAMPLE: Calculates mean runtime for Boyer_Moore 2
+    getAverageRunTime(Boyer_Moore2, 10);
+
+    // EXAMPLE: Calculates mean runtime for Rabin_Karp2
+    //int mean2 = 0;
+    //for (int i = 0; i < 10; i++) {
+        //Rabin_Karp2.search(true);
+        //mean2 += Rabin_Karp2.getRunTime();
+    //}
+    //mean2 /= 10;
+    //std::cout << "The mean runtime for Rabin_Karp2 was " << mean2 << " microseconds" << std::endl;
+}
---
+++ Version 1.0 - Command Line Tool/string_search_I.h
@@ -0,0 +1,83 @@
+#ifndef STRING_SEARCH_I_H
+#define STRING_SEARCH_I_H
+
+#include <fstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <utility>
+#include <chrono>
+
+class Rabin_Karp {
+     private :
+        std::string inputString; // can be filename or string
+        std::string searchString; //string you are searching for
+        bool isFile; // determines if string describes the name of a file or just a search string
+        std::string searchWindow;
+        double t_out; // runtime duration
+        std::string tag; // label of the object ex: "Rabin_Karp 1"
+
+
+     public :
+
+        // constructor
+        Rabin_Karp(std::string inputString, std::string searchString, bool isFile, bool isCaseSensitive); // constructor for looking through a txt file
+
+        // getters and setters
+        std::string getSearchWindow();
+        void setSearchWindow(std::string &searchWindow);
+        double getRunTime();
+        void setRunTime(double t_out);
+        std::string getTag();
+        void setTag(std::string tag);
+
+        // static functions
+        static std::string processFile(std::string &filename);
+        static long long hash(std::string &stringToHash); //find the hash value of a string or substring
+        static void output(std::string &searchString, std::list<int> &foundIndexes, int &count, std::string &tag);
+        static void countObjs();
+
+        // static variables
+        static int objCount;
+
+        // object functions
+        void findRunTime(std::chrono::steady_clock::time_point &t_start);
+        void search(bool supressOutput); //search the file or input string for the search string
+};
+
+class Boyer_Moore {
+     private :
+        std::string inputString; // can be filename or string
+        std::string searchString; //string you are searching for
+        bool isFile; // determines if string describes the name of a file or just a search string
+        double t_out; // runtime duration
+        int idx;
+        std::string tag; // label of the object ex: "Boyer_Moore 1"
+
+     public :
+        //constructor
+        Boyer_Moore(std::string inputString, std::string searchString, bool isFile, bool isCaseSensitive); // constructor for looking through a txt file
+
+        //getters and setters
+        double getRunTime();
+        void setRunTime(double t_out);
+        std::string getTag();
+        void setTag(std::string tag);
+
+        //static functions
+        static std::string processFile(std::string &filename);
+        static void output(std::string &searchString, std::list<int> &foundIndexes, int &count, std::string &tag);
+        static int badChar(int idx, std::string &inputString, std::string searchString, std::list<int> &foundIndexes, int &count, int searchLength, std::vector<std::pair<char,int>> &badCharTable);
+        static void generateBadCharTable(int searchLength, std::string &searchString, std::vector<std::pair<char,int>> &badCharTable);
+        static void countObjs();
+
+        // static variables
+        static int objCount;
+
+        //object functions
+        void findRunTime(std::chrono::steady_clock::time_point &t_start);
+        void search(bool surpressOutput); //search the file or input string for the search string
+};
+
+
+#endif // STRING_SEARCH_I_H
